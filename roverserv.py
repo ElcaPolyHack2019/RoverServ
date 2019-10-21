e7d860af91e65ddfca6d5c355dbc90f0c2b0fb16
Roemer <r.baeriswyl@gmail.com>
2019-10-21 07:37:45 +0000

    Added more methods

--- roverserv.py
+++ roverserv.py
@@ -6,6 +6,7 @@ from roverserv import Rover
 ##############################
 # Globals (cough)
 ##############################
+
 app = Flask(__name__)
 swagger = Swagger(app)
 ros = None
@@ -16,6 +17,8 @@ rovers = [
 ##############################
 # Web API
 ##############################
+
+
 @app.route("/")
 @swag_from("static/swagger-doc/main.yml")
 def main():
@@ -25,17 +28,16 @@ def main():
 @app.route("/api/<rover_id>")
 @swag_from("static/swagger-doc/rover.yml")
 def rover(rover_id: str):
-    rover = next(
-        (rover for rover in rovers if rover.rover_id == rover_id), None)
+    rover = get_rover_by_id(rover_id)
     if rover is None:
         abort(404, description=f"Rover with id {rover_id} not found.")
+    rover.update_gps()
     return jsonify(rover.to_json())
 
 
 @app.route("/api/<rover_id>/topics")
 def topics(rover_id: str):
-    rover = next(
-        (rover for rover in rovers if rover.rover_id == rover_id), None)
+    rover = get_rover_by_id(rover_id)
     if rover is None:
         abort(404, description=f"Rover with id {rover_id} not found.")
     client = get_socket(rover)
@@ -43,6 +45,15 @@ def topics(rover_id: str):
     close_client(client)
     return jsonify(topics['topics'])
 
+
+@app.route("/api/<rover_id>/forward")
+def forward(rover_id: str):
+    duration = float(is_none(request.args.get("t"), 1.0))
+    rover = get_rover_by_id(rover_id)
+    if rover is None:
+        abort(404, description=f"Rover with id {rover_id} not found.")
+    rover.drive_forward(duration)
+    return jsonify({'success': 'ok'})
 
 ##############################
 # Web Sockets
@@ -65,10 +76,26 @@ def get_topics(client):
 def close_client(client):
     client.close()
 
+##############################
+# Misc
+##############################
+
+
+def get_rover_by_id(rover_id: str):
+    return next((rover for rover in rovers if rover.rover_id == rover_id), None)
+
+
+def is_none(value, alternative):
+    if value is None:
+        return alternative
+    else:
+        return value
 
 ##############################
 # Main
 ##############################
+
+
 if __name__ == '__main__':
     port = 5000
     host = "0.0.0.0"
--- roverserv/rover.py
+++ roverserv/rover.py
@@ -3,10 +3,34 @@ class Rover:
         self.rover_id = rover_id
         self.bridge_host = bridge_host
         self.bridge_port = bridge_port
+        self.gps_x = 0
+        self.gps_y = 0
+        self.gps_orientation = 0
+
+    def drive_forward(self, duration: float):
+        # TODO
+        pass
+
+    def drive_backward(self, duration: float):
+        # TODO
+        pass
+
+    def rotate(self, angle: float):
+        # TODO
+        pass
+
+    def update_gps(self):
+        # TODO
+        self.gps_x = 0
+        self.gps_y = 0
+        self.gps_orientation = 0
 
     def to_json(self):
         return {
             'rover_id': self.rover_id,
             'bridge_host': self.bridge_host,
             'bridge_port': self.bridge_port,
+            'gps_x': self.gps_x,
+            'gps_y': self.gps_y,
+            'gps_orientation': self.gps_orientation,
         }
